eec394188ebeb915e4694663287524f5e263965e
yevheniimoskalenko <testjokerqwerty@gmail.com>
2021-02-09 18:48:03 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    312 adw
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style lang="scss">
+body {
+  font-family: "Raleway", sans-serif;
+  font-size: 24px;
+}
+</style>
